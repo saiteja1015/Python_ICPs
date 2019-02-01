20e3b22cfcbf39c574fa5ee1bf59b39508550561
saiteja1015 <42659064+saiteja1015@users.noreply.github.com>
2019-02-01 16:33:17 -0600

    ICP 1

--- letters&numbers.py
+++
@@ -1,7 +0,0 @@
-String=input("Please Enter the string:")
-numbers = sum(c.isdigit() for c in String)
-Letters   = sum(c.isalpha() for c in String)
-spaces  = sum(c.isspace() for c in String)
-print("Numbers: %s" %numbers)
-print("Letters: %d" %Letters)
-print('words : ',spaces+1)
